dad1a05ecc2eb9013283c86f067d9b6b8b9ba453
ZuzGom <74205927+ZuzGom@users.noreply.github.com>
2024-01-02 18:26:54 +0100

    dziala

--- connect.py
+++ connect.py
@@ -1,6 +1,6 @@
 import pyodbc 
 def get_cursor():
-    cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for SQL Server};Server=dbmanage.lab.ii.agh.edu.pl;Database=u_magorski;Port=1433;User ID=u_magorski;Password=LUJpqYCkfuzo')
+    cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for SQL Server};Server=dbmanage.lab.ii.agh.edu.pl;Database=u_magorski;Port=1433;User ID=u_magorski;Password=xxxxxxxx')
     return cnxn.cursor()
    
    
